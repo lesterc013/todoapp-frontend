1218edab6e1ea7717f31eaf7031d08670b8fcf45
lester <lesterc013@gmail.com>
2024-07-17 19:06:37 +0800

    Added checkbox for marking a todo as done

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,10 @@
-const Todo = ({ todo, handleDelete }) => {
+const Todo = ({ todo, handleDelete, handleCheckboxChange }) => {
   const done = todo.done ? 'done' : 'not done'
   return (
     <div>
-      {todo.task}, {done}
       <button onClick={() => handleDelete(todo.id)}>x</button>
+      {todo.task}, {done}
+      <input type='checkbox' onChange={() => handleCheckboxChange(todo)} />
     </div>
   )
 }
