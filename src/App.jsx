07e3d82f42618e67a89987c1e94626c2d8d09cf4
lester <lesterc013@gmail.com>
2024-07-17 18:27:17 +0800

    Implemented main code for rendering todos in db on first render; Implemented todo creation; Implemented rendering Todos and TodoForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,61 @@
 import { useState, useEffect } from 'react'
+import todoServices from '../services/todoServices'
+import TodoForm from './components/TodoForm'
+import Todo from './components/Todo'
 
 function App() {
+  const [todos, setTodos] = useState(null)
   const [newTodo, setNewTodo] = useState('')
 
-  return (
-    <div>
-      <h1>TODOs</h1>
-      <form>
-        <button type='submit' name='add'>
-          +
-        </button>
-        <input
-          type='text'
-          name='todo'
-          value={newTodo}
-          onChange={({ target }) => setNewTodo(target.value)}
+  // Fetch all todos related to this sessionId from the db upon first render of the app
+  // Note: sessionId SHOULD automatically get stuck as a Cookie
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const todosArr = await todoServices.getTodos()
+      setTodos(todosArr)
+    }
+    fetchTodos()
+  }, [])
+
+  const handleTodoCreation = async (event) => {
+    event.preventDefault()
+    const todo = {
+      task: newTodo,
+    }
+    const createdTodo = await todoServices.createTodo(todo)
+    // GET all the todos again from the new database, and setTodos
+    setTodos(await todoServices.getTodos())
+    setNewTodo('')
+  }
+
+  // Conditional Render -- todos has nothing, just render the TodoForm; else map todos and TodoForm
+
+  if (!todos) {
+    return (
+      <div>
+        <h1>TODOs</h1>
+        <TodoForm
+          handleTodoCreation={handleTodoCreation}
+          inputValue={newTodo}
+          setNewTodo={setNewTodo}
         />
-      </form>
-    </div>
-  )
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>TODOs</h1>
+        {todos.map((eachTodo) => (
+          <Todo todo={eachTodo} key={eachTodo.id} />
+        ))}
+        <TodoForm
+          handleTodoCreation={handleTodoCreation}
+          inputValue={newTodo}
+          setNewTodo={setNewTodo}
+        />
+      </div>
+    )
+  }
 }
 
 export default App
