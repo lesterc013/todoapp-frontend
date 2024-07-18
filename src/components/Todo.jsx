a6ccb5c40a71fc4b2dba5abb15fa59157b87e18d
lester <lesterc013@gmail.com>
2024-07-18 16:51:22 +0800

    Incorporated react-boostrap components and todoStyles.css

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+import '../../styles/todoStyles.css'
 
 const Todo = ({ todo, handleDelete, handleTodoUpdate }) => {
-  // Initial value of todoTaskUpdate should be todo.task?
   const [todoTaskUpdate, setTodoTaskUpdate] = useState(todo.task)
-  // const [todoDoneUpdate, setTodoDoneUpdate] = useState(todo.done)
-  // const done = todo.done ? 'done' : 'not done'
 
   const blurInputOnEnterOrEscape = (event) => {
     if (event.key === 'Enter' || event.key === 'Escape') {
@@ -29,10 +29,12 @@ const Todo = ({ todo, handleDelete, handleTodoUpdate }) => {
   }
 
   return (
-    <div>
-      {/* <input type='checkbox' onChange={() => handleCheckboxChange(todo)} /> */}
-      <button onClick={submitTodoDoneUpdate}>✓</button>
+    <Container className='container'>
+      <Button className='btn updateBtn' onClick={submitTodoDoneUpdate}>
+        ✓
+      </Button>
       <input
+        className='todoInput'
         type='text'
         name='todoInDb'
         value={todoTaskUpdate}
@@ -40,8 +42,14 @@ const Todo = ({ todo, handleDelete, handleTodoUpdate }) => {
         onKeyDown={blurInputOnEnterOrEscape}
         onBlur={submitTodoTaskUpdate}
       />
-      <button onClick={() => handleDelete(todo.id)}>x</button>
-    </div>
+      <Button
+        className='btn delBtn'
+        size='sm'
+        onClick={() => handleDelete(todo.id)}
+      >
+        del
+      </Button>
+    </Container>
   )
 }
 
