da91b455c34ab4f94ba7477f61b705e9e1a374a0
lester <lesterc013@gmail.com>
2024-07-18 11:38:00 +0800

    Every Todo is now an input that allows users to update the task and doneness

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,46 @@
-const Todo = ({ todo, handleDelete, handleCheckboxChange }) => {
-  const done = todo.done ? 'done' : 'not done'
+import { useState } from 'react'
+
+const Todo = ({ todo, handleDelete, handleTodoUpdate }) => {
+  // Initial value of todoTaskUpdate should be todo.task?
+  const [todoTaskUpdate, setTodoTaskUpdate] = useState(todo.task)
+  // const [todoDoneUpdate, setTodoDoneUpdate] = useState(todo.done)
+  // const done = todo.done ? 'done' : 'not done'
+
+  const blurInputOnEnterOrEscape = (event) => {
+    if (event.key === 'Enter' || event.key === 'Escape') {
+      event.target.blur()
+    }
+  }
+
+  const submitTodoTaskUpdate = () => {
+    const update = {
+      task: todoTaskUpdate,
+      done: todo.done,
+    }
+    handleTodoUpdate(todo, update)
+  }
+
+  const submitTodoDoneUpdate = () => {
+    const update = {
+      task: todoTaskUpdate,
+      done: !todo.done,
+    }
+    handleTodoUpdate(todo, update)
+  }
+
   return (
     <div>
+      {/* <input type='checkbox' onChange={() => handleCheckboxChange(todo)} /> */}
+      <button onClick={submitTodoDoneUpdate}>✓</button>
+      <input
+        type='text'
+        name='todoInDb'
+        value={todoTaskUpdate}
+        onChange={({ target }) => setTodoTaskUpdate(target.value)}
+        onKeyDown={blurInputOnEnterOrEscape}
+        onBlur={submitTodoTaskUpdate}
+      />
       <button onClick={() => handleDelete(todo.id)}>x</button>
-      {todo.task}, {done}
-      <input type='checkbox' onChange={() => handleCheckboxChange(todo)} />
     </div>
   )
 }
