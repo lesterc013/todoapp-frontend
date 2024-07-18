9d6577b96ec6a0fc0601b29cf628ebf34aaccbb4
lester <lesterc013@gmail.com>
2024-07-18 23:11:30 +0800

    Added svg of + sign

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,6 +1,6 @@
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
-import '../../styles/todoFormStyles.css'
+import '../../styles/styles.css'
 
 const TodoForm = ({ handleTodoCreation, inputValue, setNewTodo }) => {
   return (
@@ -15,7 +15,19 @@ const TodoForm = ({ handleTodoCreation, inputValue, setNewTodo }) => {
           autoComplete='off'
         />
         <Button className='addBtn' type='submit' name='add'>
-          add
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='20'
+            height='20'
+            fill='currentColor'
+            className='bi bi-plus-lg'
+            viewBox='0 0 16 16'
+          >
+            <path
+              fillRule='evenodd'
+              d='M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2'
+            />
+          </svg>
         </Button>
       </Container>
     </form>
