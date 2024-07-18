682fa8c14a4b2ea1ca8a7453a32f38c87ff9a4fb
lester <lesterc013@gmail.com>
2024-07-18 18:06:57 +0800

    Imported react-bootstrap components required for CSS

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,15 +1,22 @@
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+import '../../styles/todoFormStyles.css'
+
 const TodoForm = ({ handleTodoCreation, inputValue, setNewTodo }) => {
   return (
     <form onSubmit={handleTodoCreation}>
-      <button type='submit' name='add'>
-        +
-      </button>
-      <input
-        type='text'
-        name='todo'
-        value={inputValue}
-        onChange={({ target }) => setNewTodo(target.value)}
-      />
+      <Container className='formContainer'>
+        <input
+          className='formInput'
+          type='text'
+          name='todo'
+          value={inputValue}
+          onChange={({ target }) => setNewTodo(target.value)}
+        />
+        <Button className='addBtn' type='submit' name='add'>
+          add
+        </Button>
+      </Container>
     </form>
   )
 }
